0590ed25b0b87dcaa90ac20122a1b205457b215e
Thayson Guedes <83515833+ThaysonScript@users.noreply.github.com>
2023-11-17 20:03:44 -0300

    Create manipularArquivo.h

---
+++ includes/manipularArquivo.h
@@ -0,0 +1,19 @@
+/*
+
+FILE *fptr
+fptr = typeRead(filename, mode);
+
+w - Writes to a file
+a - Appends new data to a file
+r - Reads from a file
+
+fopen() == abrir arquivo
+fclose(fptr) == fechar arquivo
+
+*/
+
+// manipular arquivos
+void criarArquivo();
+void lerArquivo();
+void atualizarArquivo();
+void deletarArquivo();
